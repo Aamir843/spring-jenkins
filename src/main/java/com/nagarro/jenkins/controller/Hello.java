845c4df7907d2ce143a658d095bb3f2f884db0b7
Aamir <aamirakhtaramu@gmail.com>
2023-12-22 07:07:06 +0530

    git commit -m "Fifth commit"

--- src/main/java/com/nagarro/jenkins/controller/Hello.java
+++ src/main/java/com/nagarro/jenkins/controller/Hello.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Hello {
 
-    @GetMapping("/hello")
+    @GetMapping("hello")
     String hello(){
         return "Hello Jenkins";
     }
